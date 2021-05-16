69d6fef4ecc1c2d9fb2109222e12c2d79beb8104
Aleksandr Makarov <aleksandermack4r0ff@yandex.ru>
2021-05-16 21:01:40 +0200

    added full adaptive

--- script.js
+++ script.js
@@ -17,66 +17,42 @@ function handleOverlayClick(event) {
 }
 popup.addEventListener('click', handleOverlayClick);
 
+//находим строку в DOM с именем и профессией
+const nameInMarkup = document.querySelector('.profile__name').textContent;//Жак-Ив Кусто
+const professionInMarkup = document.querySelector('.profile__profession').textContent;// Исследователь океана
 
-
-
-
-
-/*
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.profile__name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.profile__profession');// Воспользуйтесь инструментом .querySelector()
-
+//находим форму попАп
+const formElement = document.querySelector('.popup__form-container');
+//находим поле ввода имени и профессии в попАпе
+const nameInput = document.querySelector('.popup__input-name');
+const jobInput = document.querySelector('.popup__input-profession');
+//делаем значение в поле ввода формы равным значению в имени и профессии в разметке
+nameInput.value = nameInMarkup;
+jobInput.value = professionInMarkup;
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    let nameValue = nameInput.getAttribute('value');
-    let jobValue = JobInput.getAttribute('value');
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__profession');
-    // Вставьте новые значения с помощью textContent
-    profileName.textContent = nameValue;
-    profileJob.textContent = jobValue;
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-*/
-
-
-
-
-//8. Получение имени и информации о себе в поля ввода
-let infoName = document.querySelector('.profile__name').textContent;
-let infoJob = document.querySelector('.profile__profession').textContent;
-const nameProfile = document.forms.profile.elements.name;
-const aboutProfile = document.forms.profile.elements.about;
-
-nameProfile.value = infoName;
-aboutProfile.value = infoJob;
-
 //Смена контента на введённое значение в поля формы по клику на кнопку "Сохранить"
-const popupEditButton = document.querySelector('.popup__button-save');
+const popupButtonSave = document.querySelector('.popup__button-save');
 
-popupEditButton.addEventListener('click', function(event) {
-    const userName = document.querySelector('.profile__name');
-    const userJob = document.querySelector('.profile__profession');
-    userName.textContent = nameProfile.value;
-    userJob.textContent = aboutProfile.value;
-
+popupButtonSave.addEventListener('click', function(event){
+    nameInput.value = nameInMarkup;
+    jobInput.value = professionInMarkup;
     //запрещаем перезагрузку страницы
 	event.preventDefault();
     //закрываем попап при событии click
-	deleteClassToPopupEdit();
+	popupButtonSave.addEventListener('click', togglePopup);
 });
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+//лайк активен
+const elementLike = document.querySelector('.element__like');
+function toggleLike(event) {
+    event.preventDefault();
+    elementLike.classList.toggle('element__like-on')
+}
+elementLike.addEventListener('click', toggleLike);
